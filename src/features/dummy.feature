498c0f586ca7ca204ef8e5e6a6b5cb75810d0c96
Dan Rusu <dangingko@yahoo.com>
2024-06-17 23:16:33 +0300

    scenario outline & table data poc

--- src/features/dummy.feature
+++ src/features/dummy.feature
@@ -2,13 +2,18 @@ Feature: Showing off behave
 
   Scenario: Run a simple test
     Given we set dummy preconditions
+      | user | id   |
+      | dan  | 1111 |
+      | joe  | 2222 |
+
     When we do 5 dummy actions
     Then we get dummy result
 
-# Scenario Outline: Data Driven <index>
-#   Given we display message <message>
+  Scenario Outline: Data Driven
+    Given we <message>
+    Then index is <index>
 
-#   Examples:
-#     | index | message   |
-#     | 1     | behave    |
-#     | 2     | misbehave |
+    Examples:
+      | message   | index |
+      | behave    | 1     |
+      | misbehave | 2     |
--- src/features/steps/dummy_steps.py
+++ src/features/steps/dummy_steps.py
@@ -1,7 +1,9 @@
 from behave import given, when, then, step
     
 @given('we set dummy preconditions')
-def step_impl(context):  
+def step_impl(context):
+  for row in context.table.rows:
+    print(f'{row[0]} : {row[1]}')
   pass
 
 @when('we do {count:d} dummy actions')
@@ -14,10 +16,14 @@ def step_impl(context, count):  # -- NOTE: number is converted into integer
 @then('we get dummy result')
 def step_impl(context):  
   print(context.actions_count)
-  assert context.failed is False
+  assert context.failed is False  
   assert context.actions_count > 0
 
-# @given('we display message {message}')
-# def step_impl(context, index, message):  
-#   print(f'{index} - ${message}')
+# scenario outline step
+@given('we {message}')
+def step_impl(context, message):  
+  print(f'mssage: ${message}')
   
+@then('index is {index:d}')
+def step_impl(context, index):  
+  print(f'index: ${index}')
